91ff003825fcd09f2de741cc8c0aac5be52003d0
abhirup17 <abhiruprulz@gmail.com>
2020-10-31 23:48:29 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -18,7 +18,7 @@ class BinarySearchExample
 {
    public static void main(String args[])
    {
-      int counter, num, item, array[], first, last, middle;
+      int counter, num, item, array[], first, last, m;
       //To capture user input
       Scanner input = new Scanner(System.in);
       System.out.println("Enter number of elements:");
@@ -32,27 +32,27 @@ class BinarySearchExample
       for (counter = 0; counter < num; counter++)
           array[counter] = input.nextInt();
 
-      System.out.println("Enter the search value:");
+      System.out.println("Enter  value:");
       item = input.nextInt();
       first = 0;
       last = num - 1;
-      middle = (first + last)/2;
+      m = (first + last)/2;
       while( first <= last )
       {
-         if ( array[middle] < item )
-           first = middle + 1;
-         else if ( array[middle] == item )
+         if ( array[m] < item )
+           first = m + 1;
+         else if ( array[m] == item )
          {
-           System.out.println(item + " found at location " + (middle + 1) + ".");
+           System.out.println(item + " found at location " + (m + 1) + ".");
            break;
          }
          else
          {
-            printf("helo");
+            printf("bye");
                
-             last = middle - 1;
+             last = m - 1;
          }
-         middle = (first + last)/2;
+         m = (first + last)/2;
       }
       if ( first > last )
           System.out.println(item + " is not found.\n");
